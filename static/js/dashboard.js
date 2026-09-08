5a147b82e9fa5f1dc1406c7e5f4a99e6a3d8a59f
PCHARKHA <pranjal.charkha37@gmail.com>
2026-09-08 11:13:17 +0530

    Home page and dashboard page logic created

---
+++ static/js/dashboard.js
@@ -0,0 +1,139 @@
+async function loadDashboard() {
+    try {
+        const response = await fetch("/expenses");
+        const expenses = await response.json();
+    
+        calculateSummary(expenses);
+        displayRecentExpenses(expenses);
+    
+        }catch (error) {
+            console.error("Error loading dashboard:", error);
+        }
+    
+    }
+
+function calculateSummary(expenses){
+    const todayTotalElement = document.getElementById("today-total");
+    const weekTotalElement = document.getElementById("week-total");
+    const monthTotalElement = document.getElementById("month-total");
+
+    const today = new Date();
+
+    let todayTotal = 0;
+    let weekTotal = 0;
+    let monthTotal = 0;
+
+    const startOfWeek = new Date(today);
+    const day = today.getDay();
+    startOfWeek.setDate(today.getDate() - day);
+    startOfWeek.setHours(0, 0, 0, 0);
+    
+    expenses.forEach(function (expense) {
+        const expenseDate = expense.date;
+        const todayDate = today.toISOString().split("T")[0];
+
+        const expenseDateObject = new Date(expense.date);
+
+        const expenseMonth = expenseDateObject.getMonth();
+        const expenseYear = expenseDateObject.getFullYear();
+
+        const currentMonth = today.getMonth();
+        const currentYear = today.getFullYear();
+
+        if (expenseDate === todayDate) {
+                todayTotal = todayTotal + expense.amount;
+        }
+
+        // This week's expense
+        if (expenseDateObject >= startOfWeek && expenseDateObject <= today) {
+                 weekTotal = weekTotal + expense.amount;
+        }
+        //This months's expense
+        if (expenseMonth === currentMonth && expenseYear === currentYear) {
+                monthTotal = monthTotal + expense.amount;
+            }
+        });
+        
+        todayTotalElement.textContent = `₹ ${todayTotal}`;
+        weekTotalElement.textContent = `₹ ${weekTotal}`;
+        monthTotalElement.textContent = `₹ ${monthTotal}`;
+}
+//Working on add expense button
+const addExpenseButton = document.getElementById("add-expense-btn");
+
+addExpenseButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "/add-expense";
+
+});
+
+
+function displayRecentExpenses(expenses){
+    const expenseList = document.getElementById("expense-list");
+    expenseList.innerHTML = "";
+
+    const recentExpenses = expenses.slice(-5).reverse();
+
+    recentExpenses.forEach(function (expense) {
+        const expenseItem = document.createElement("div");
+        expenseItem.classList.add("expense-item");
+
+        const categoryElement = document.createElement("span");
+        categoryElement.classList.add("category-tag");
+        // Code to add category-specific css
+        const categoryClass = "category-" + expense.category.toLowerCase();
+        categoryElement.classList.add(categoryClass);
+
+        categoryElement.textContent = expense.category;
+
+
+        // Note
+        const noteElement = document.createElement("span");
+        noteElement.classList.add("expense-note");
+        noteElement.textContent = expense.note;
+
+
+        // Amount
+        const amountElement = document.createElement("span");
+        amountElement.classList.add("expense-amount");
+        amountElement.textContent = `₹ ${expense.amount}`;
+
+
+        // Date
+        const dateElement = document.createElement("span");
+        dateElement.classList.add("expense-date");
+        dateElement.textContent = formatExpenseDate(expense.date);
+
+        //Appending to expenseItem
+        expenseItem.appendChild(categoryElement);
+        expenseItem.appendChild(noteElement);
+        expenseItem.appendChild(amountElement);
+        expenseItem.appendChild(dateElement);
+
+        expenseList.appendChild(expenseItem);
+    });
+}
+
+
+function formatExpenseDate(expenseDate) {
+    const today = new Date();
+    const expenseDateObject = new Date(expenseDate);
+
+    today.setHours(0, 0, 0, 0);
+    expenseDateObject.setHours(0, 0, 0, 0);
+
+    const differenceInMilliseconds = today - expenseDateObject;
+    const differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+     
+    if (differenceInDays === 0) {
+        return "Today";
+    }
+
+    if (differenceInDays === 1) {
+        return "Yesterday";
+    }
+
+    return `${differenceInDays} days ago`;
+}
+
+loadDashboard();
